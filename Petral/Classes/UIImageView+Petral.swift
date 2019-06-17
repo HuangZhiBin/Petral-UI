5903b01b67be7acd9a4125d7be37683d52e7330e
黄智彬 <ikrboy@163.com>
2019-06-17 20:19:26 +0800

    Update UIImageView+Petral.swift

--- Petral/Classes/UIImageView+Petral.swift
+++ Petral/Classes/UIImageView+Petral.swift
@@ -10,32 +10,38 @@ import UIKit
 
 extension UIImageView {
     
+    @discardableResult
     public func pt_image(_ image: UIImage) -> UIImageView {
         self.image = image;
         return self;
     }
     
+    @discardableResult
     override public func pt_frame(x: CGFloat, y: CGFloat, width: CGFloat, height: CGFloat) -> UIImageView {
         self.frame = CGRect.init(x: x, y: y, width: width, height: height);
         return self;
     }
     
+    @discardableResult
     override public func pt_backgroundColor(_ color: UIColor) -> UIImageView {
         self.backgroundColor = color;
         return self;
     }
     
+    @discardableResult
     override public func pt_borderRadius(_ radius: CGFloat) -> UIImageView {
         self.layer.masksToBounds = true;
         self.layer.cornerRadius = radius;
         return self;
     }
     
+    @discardableResult
     override public func pt_borderWidth(_ borderWidth: CGFloat) -> UIImageView {
         self.layer.borderWidth = borderWidth;
         return self;
     }
     
+    @discardableResult
     override public func pt_borderColor(_ borderColor: UIColor) -> UIImageView {
         self.layer.borderColor = borderColor.cgColor;
         return self;
