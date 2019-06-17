02cd6a059b8a1e30c9c6d593067abd9345e18a2c
黄智彬 <ikrboy@163.com>
2019-06-17 20:21:10 +0800

    Update UIView+Petral.swift

--- Petral/Classes/UIView+Petral.swift
+++ Petral/Classes/UIView+Petral.swift
@@ -60,46 +60,55 @@ extension UIView : PetralViewProtocol{
         }
     }
     
+    @discardableResult
     public func pt_frame(x: CGFloat, y: CGFloat, width: CGFloat, height: CGFloat) -> UIView {
         self.frame = CGRect.init(x: x, y: y, width: width, height: height);
         return self;
     }
     
+    @discardableResult
     public func pt_backgroundColor(_ color: UIColor) -> UIView {
         self.backgroundColor = color;
         return self;
     }
     
+    @discardableResult
     public func pt_borderRadius(_ radius: CGFloat) -> UIView {
         self.layer.cornerRadius = radius;
         return self;
     }
     
+    @discardableResult
     public func pt_borderWidth(_ borderWidth: CGFloat) -> UIView {
         self.layer.borderWidth = borderWidth;
         return self;
     }
     
+    @discardableResult
     public func pt_borderColor(_ borderColor: UIColor) -> UIView {
         self.layer.borderColor = borderColor.cgColor;
         return self;
     }
     
+    @discardableResult
     public func pt_shadowColor(_ shadowColor: UIColor) -> UIView {
         self.layer.shadowColor = shadowColor.cgColor;
         return self;
     }
     
+    @discardableResult
     public func pt_shadowOpacity(_ shadowOpacity: Float) -> UIView {
         self.layer.shadowOpacity = shadowOpacity;
         return self;
     }
     
+    @discardableResult
     public func pt_shadowRadius(_ shadowRadius: CGFloat) -> UIView {
         self.layer.shadowRadius = shadowRadius;
         return self;
     }
     
+    @discardableResult
     public func pt_shadowOffset(_ shadowOffset: CGSize) -> UIView {
         self.layer.shadowOffset = shadowOffset;
         return self;
