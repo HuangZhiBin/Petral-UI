76ade88d36ce4cc863e85856f3fc488093a9e5c0
黄智彬 <ikrboy@163.com>
2019-06-17 20:19:08 +0800

    Update UIButton+Petral.swift

--- Petral/Classes/UIButton+Petral.swift
+++ Petral/Classes/UIButton+Petral.swift
@@ -10,57 +10,68 @@ import UIKit
 
 extension UIButton {
     
+    @discardableResult
     public func pt_font(size: CGFloat, bold: Bool) -> UIButton {
         self.titleLabel?.font = bold == true ? UIFont.boldSystemFont(ofSize: size) : UIFont.systemFont(ofSize: size);
         return self;
     }
     
+    @discardableResult
     public func pt_titleColor(_ titleColor: UIColor, state: UIControl.State) -> UIButton {
         self.setTitleColor(titleColor, for: state);
         return self;
     }
     
+    @discardableResult
     public func pt_backgroundColor(_ color: UIColor, state: UIControl.State) -> UIButton {
         self.setBackgroundImage(self.createImageWithColor(color: color), for: state);
         return self;
     }
     
+    @discardableResult
     public func pt_backgroundImage(_ image: UIImage, state: UIControl.State) -> UIButton {
         self.setBackgroundImage(image, for: state);
         return self;
     }
     
+    @discardableResult
     public func pt_title(_ title: String, state: UIControl.State) -> UIButton {
         self.setTitle(title, for: state)
         return self;
     }
     
+    @discardableResult
     public func pt_align(_ align: NSTextAlignment) -> UIButton{
         self.titleLabel?.textAlignment = align;
         return self;
     }
     
+    @discardableResult
     override public func pt_frame(x: CGFloat, y: CGFloat, width: CGFloat, height: CGFloat) -> UIButton {
         self.frame = CGRect.init(x: x, y: y, width: width, height: height);
         return self;
     }
     
+    @discardableResult
     override public func pt_backgroundColor(_ color: UIColor) -> UIButton {
         self.backgroundColor = color;
         return self;
     }
     
+    @discardableResult
     override public func pt_borderRadius(_ radius: CGFloat) -> UIButton {
         self.layer.masksToBounds = true;
         self.layer.cornerRadius = radius;
         return self;
     }
     
+    @discardableResult
     override public func pt_borderWidth(_ borderWidth: CGFloat) -> UIButton {
         self.layer.borderWidth = borderWidth;
         return self;
     }
     
+    @discardableResult
     override public func pt_borderColor(_ borderColor: UIColor) -> UIButton {
         self.layer.borderColor = borderColor.cgColor;
         return self;
